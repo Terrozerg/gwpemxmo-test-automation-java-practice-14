02edf186add9ec24c384ff2915ca048fc3c797a4
Stanislav <terrozerg@gmail.com>
2021-06-06 12:00:24 +0300

    1

--- src/main/java/com/epam/test/automation/java/practice14/advanced/task15/Task15.java
+++ src/main/java/com/epam/test/automation/java/practice14/advanced/task15/Task15.java
@@ -1,11 +1,15 @@
 package com.epam.test.automation.java.practice14.advanced.task15;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Task15 {
-    private Task15(){}
+    private Task15() {
+    }
 
     public static List<CountryStat> name(List<Good> goodList, List<StorePrice> storePriceList) {
         return goodList.stream()
@@ -40,6 +44,7 @@ public class Task15 {
                                 .min(BigDecimal::compareTo)
                                 .orElse(BigDecimal.ZERO))
                 )
+                .sorted(Comparator.comparing(CountryStat::getCountryOfOrigin))
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/com/epam/test/automation/java/practice14/middle/Task8.java
+++ src/main/java/com/epam/test/automation/java/practice14/middle/Task8.java
@@ -3,7 +3,6 @@ package com.epam.test.automation.java.practice14.middle;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Task8 {
     private Task8(){}
